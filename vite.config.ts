438a30f7b6460d421caf752ac27d6daaf2f06d4f
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2026-02-11 00:56:27 +0100

    feat: Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/web-invoice/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
